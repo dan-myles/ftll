67d9060975c070486eac6361f570a08cf22ea45a
danlikestocode <79137382+danlikestocode@users.noreply.github.com>
2024-04-03 14:55:02 -0700

    chore: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+var OFF = 0,
+  WARN = 1,
+  ERROR = 2
+
+module.exports = exports = {
+  root: true,
+  extends: "next/core-web-vitals",
+  rules: {
+    eqeqeq: [WARN, "smart"],
+    curly: [WARN, "all"],
+    "capitalized-comments": [WARN, "always"],
+    "no-else-return": [ERROR, { allowElseIf: false }],
+    "no-lonely-if": [WARN],
+    "no-unneeded-ternary": [ERROR, { defaultAssignment: false }],
+  },
+}
