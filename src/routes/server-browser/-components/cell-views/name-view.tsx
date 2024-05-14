3ee828daac4a332ba5c447792cad6713d2dfe552
danlikestocode <79137382+danlikestocode@users.noreply.github.com>
2024-05-13 19:22:54 -0700

    fix(browser): overscroll on long modnames

--- src/routes/server-browser/-components/cell-views/name-view.tsx
+++ src/routes/server-browser/-components/cell-views/name-view.tsx
@@ -8,38 +8,17 @@ interface NameViewProps<TData> {
 export function NameView<TData>({ row }: NameViewProps<TData>) {
   const server = row.original as Server32
 
-  // Theres gotta be a better way to do this 💀
-  if (!server.mod_list) {
-    return (
-      <div className="flex flex-col">
-        <div
-          className="truncate font-medium md:max-w-[350px] lg:max-w-[600px]
-            xl:max-w-[850px]"
-        >
-          {server.name}
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div
-      className="flex max-w-[350px] flex-col lg:max-w-[600px] xl:max-w-[850px]"
+      className="flex max-w-[350px] flex-col lg:max-w-[450px] xl:max-w-[550px]"
     >
-      <div
-        className="truncate font-medium md:max-w-[350px] lg:max-w-[600px]
-          xl:max-w-[850px]"
-      >
-        {server.name}
-      </div>
+      <div className="truncate font-medium">{server.name}</div>
       <div className="truncate text-gray-500">
-        {server.mod_list.map((mod) => {
-          return (
-            <span key={mod.workshop_id} className="text-gray-500">
-              {mod.name} |{" "}
-            </span>
-          )
-        })}
+        {server.mod_list?.map((mod) => (
+          <span key={mod.workshop_id} className="text-gray-500">
+            {mod.name} |{" "}
+          </span>
+        ))}
       </div>
     </div>
   )
