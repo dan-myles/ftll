7635d3d234d0c573028805a342e07931b08ea833
avvo-na <79137382+danlikestocode@users.noreply.github.com>
2024-05-25 20:57:55 -0700

    build(www): aws lambda deployment

--- www/sst.config.ts
+++ www/sst.config.ts
@@ -2,7 +2,7 @@
 export default $config({
   app(input) {
     return {
-      name: "ftl-web",
+      name: "ftll-web",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
       providers: { cloudflare: true },
@@ -10,7 +10,7 @@ export default $config({
   },
 
   async run() {
-    new sst.aws.Nextjs("FTLWeb", {
+    new sst.aws.Nextjs("FTLLWeb", {
       domain: {
         name: $app.stage === "prod" ? "ftll.io" : `${$app.stage}.ftll.io`,
         dns: sst.cloudflare.dns(),
