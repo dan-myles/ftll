f9effd544d2664277c09ddeb755338d2acefc249
avvo-na <79137382+danlikestocode@users.noreply.github.com>
2024-05-25 18:02:43 -0700

    build(www): change ssm prefix aws

--- www/sst.config.ts
+++ www/sst.config.ts
@@ -1,15 +1,14 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
       name: "ftl-web",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
-    }
+      providers: { cloudflare: true },
+    };
   },
-
   async run() {
-    new sst.aws.Nextjs("FTLWeb")
+    new sst.aws.Nextjs("FTLWeb");
   },
-})
+});
